8c3a3b18aa47d150024ba8e5e1423f2032f53ac5
sheyzi <gistkiosk@gmail.com>
2022-12-21 19:26:52 +0100

    Fixed og

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,7 +23,7 @@
             name="og:description"
             content="Get a top-quality software engineering diploma without breaking the bank. Our program allows you to pay tuition only when you get hired, so you can focus on your studies and future career without financial stress. Enroll now and take the first step towards a rewarding career in software engineering."
     />
-<!--    <meta name="og:image" content="https://cooversa.com/Cooversa.jpg" />-->
+    <meta name="og:image" content="https://cooversa.com/about-us.png" />
     <meta name="og:url" content="https://cooversa.com" />
     <meta name="og:site_name" content="Cooversa" />
     <meta name="og:type" content="website" />
@@ -35,7 +35,7 @@
             name="twitter:description"
             content="Get a top-quality software engineering diploma without breaking the bank. Our program allows you to pay tuition only when you get hired, so you can focus on your studies and future career without financial stress. Enroll now and take the first step towards a rewarding career in software engineering."
     />
-<!--    <meta name="twitter:image" content="https://Cooversa.com/Cooversa.jpg" />-->
+    <meta name="twitter:image" content="https://cooversa.com/about-us.png" />
     <meta name="twitter:url" content="https://cooversa.com" />
     <meta name="twitter:image:alt" content="Cooversa" />
 </svelte:head>
