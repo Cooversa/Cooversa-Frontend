2e01132f2c86ce17bed539ef36010ff5bbac3d61
sheyzi <gistkiosk@gmail.com>
2023-01-02 01:00:51 +0100

    Made LMS available for mobile

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -54,9 +54,9 @@
 
 
 
-<div class="hidden relative min-h-screen md:flex">
+<div class="relative min-h-screen md:flex">
     <Sidebar on:navState={setSidebarState}/>
-    <div class="h-screen p w-full {isSidebarOpen ? 'ml-72' : 'ml-16'} transition-all">
+    <div class=" my-16 p w-full {isSidebarOpen ? 'md:ml-72' : 'md:ml-16'} ml-0 transition-all">
         <AppNavbar/>
         <div class="p-7">
             {#if $loading}
@@ -78,15 +78,5 @@
 
             <slot/>
         </div>
-    </div>
-</div>
-
-<div class="md:hidden flex flex-col h-screen justify-center items-center text-lg">
-    <div class="flex flex-col items-center space-y-5">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
-        </svg>
-
-        Open this page on your laptop
     </div>
 </div>
--- src/routes/(app)/dashboard/components/Banner.svelte
+++ src/routes/(app)/dashboard/components/Banner.svelte
@@ -1,10 +1,24 @@
-<div class="rounded p-10 relative" style="background-image: url('/pattern.png'); background-repeat: round;">
-    <div class="absolute top-0 opacity-5 rounded bottom-0 left-0 right-0 bg-gray-600" style="z-index: -1;">
+<div class="rounded md:p-10 p-5 relative wrapper">
 
-    </div>
-    <div style="z-index: 9999;">
-        <h1 class="text-3xl mb-5 font-bold">Welcome to the Cooversa LMS!</h1>
-        <p class="text-[#454545]">You are now enrolled in the Fundamental Track. Let's get started on your journey to becoming a top-notch software engineer.</p>
+    <div style="z-index: 9999; ">
+        <h1 class="md:text-3xl text-xl mb-5 font-bold">Welcome to the Cooversa LMS!</h1>
+        <p>You are now enrolled in the Fundamental Track. Let's get started on your journey to becoming a top-notch software engineer.</p>
     </div>
 </div>
 
+<style>
+    .wrapper {
+        background-image: url('/pattern.png');
+        background-repeat: round;
+        color: #454545;
+    }
+
+    @media (max-width: 768px) {
+        .wrapper {
+            /*background-image: none;*/
+            /*background-color: #5C57FF;*/
+            /*color: #F5F5F5;*/
+            background-size: cover;
+        }
+    }
+</style>
